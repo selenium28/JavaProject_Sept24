49740ca24b9d81e0ca87aab3e3bcc638f0415972
selenium28 <devselenium.class2819@gmail.com>
2024-09-09 20:36:25 +0530

    Data Types

---
+++ src/core/DataTypeClass.java
@@ -0,0 +1,45 @@
+package core;
+
+public class DataTypeClass { // class body
+
+	public static void main(String[] args) { // method body 
+		
+//		1. int -> values, its positive or negative values. 
+//		int -> Data types, a -> variable, 10 -> value
+		int a = 10;
+		int m;
+		m = 20;
+		System.out.println(a);
+		System.out.println(m);
+//		int a = 30;  // Duplicate local variable b  - In java duplicate 
+//		variables are not allowed
+//		int n = 50.00;  - double digit - we can't use double number in int.
+		int n = -20;
+		
+//		2. double
+		double d = 33.33;
+		System.out.println(d);
+		double d1 = 20;
+		System.out.println(d1);
+		
+//		3. char  -> it is use for single char.
+		char c = 'A';
+		System.out.println(c);
+		
+//		4. boolean -> true or false
+//		What is true & false ? -> true & false are the vales in Java 
+//		as well as keywords in java.
+		boolean b = true;
+		boolean b1 = false;
+		
+//		5. String - sequence of char. - String is a class in Java
+		String s = "Java";
+		String s1 = "d";
+		String s2 = "100 ";
+		System.out.println(s2);
+		String s3 = "200";
+		System.out.println(s2+s3);
+		
+	}
+
+}
